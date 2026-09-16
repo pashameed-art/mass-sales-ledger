cdf83b2b16dc2a2ad5debe20aba328612a6cba66
pashameed-art <pashameed@gmail.com>
2026-09-16 23:47:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mass-sales-ledger-v1.2.0';
+const C='mass-sales-ledger-v1.3.0';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
