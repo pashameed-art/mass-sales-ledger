318978bb2ce8bd60108ac015a1ab724b484adf1b
pashameed-art <pashameed@gmail.com>
2026-09-19 20:01:08 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mass-sales-ledger-v1.7-final-safe';
+const CACHE='mass-sales-ledger-v1.8.0';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
